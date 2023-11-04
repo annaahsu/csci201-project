5eb12399656c80a18344540c1995606cbd5cf211
James Li <jameslicontact@gmail.com>
2023-11-03 20:29:20 -0700

    Base framework

---
+++ backend/src/AuthThread.java
@@ -0,0 +1,11 @@
+/**
+ * HTTP server that verifies authentication requests
+ */
+
+public class AuthThread extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Main entrypoint of the backend - manages the various threads that serve the sync server and auth server
+ */
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Starting backend...");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        ExecutorService threadpool = Executors.newCachedThreadPool();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        threadpool.execute(new AuthThread());
+        threadpool.execute(new SyncThread());
     }
 }
