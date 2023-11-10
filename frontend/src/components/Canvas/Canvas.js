6ce312ff4d856e8b5a10bddfd1215f8255cccbc3
Anna Hsu <adhsu@usc.edu>
2023-11-09 20:30:15 -0800

    add primitive canvas functionality

--- frontend/src/components/Canvas/Canvas.js
+++ frontend/src/components/Canvas/Canvas.js
@@ -1,8 +1,103 @@
+"use client";
+
+import { useRef, useState } from "react";
+import Palette from "@/components/Palette/Palette";
 import styles from "./Canvas.module.css";
 
 export default function Canvas() {
+  const colors = [
+    "#000000",
+    "#ffffff",
+    "#00ff00",
+    "#0000ff",
+    "#00ffff",
+    "#ff0000",
+    "#ff00ff",
+    "#ff9f00",
+    "#ffff00",
+    "#d9d9d9",
+  ];
+  const widths = [1, 2, 4];
+
+  let changes = [];
+
+  const canvasRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [prevX, setPrevX] = useState(0);
+  const [prevY, setPrevY] = useState(0);
+  const [lineColor, setLineColor] = useState(colors[0]);
+  const [lineWidth, setLineWidth] = useState(widths[0]);
+  const width = 360;
+  const height = 240;
+
+  const handleMouseDown = (e) => {
+    setIsDrawing(true);
+    setPrevX(e.nativeEvent.offsetX);
+    setPrevY(e.nativeEvent.offsetY);
+  };
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDrawing) return;
+
+    const currentX = e.nativeEvent.offsetX;
+    const currentY = e.nativeEvent.offsetY;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = lineWidth;
+    ctx.lineJoin = "bevel";
+    ctx.lineCap = "round";
+    ctx.imageSmoothingEnabled = false;
+
+    const container = document.getElementById("canvas");
+    const scaleX = container.offsetWidth / width;
+    const scaleY = container.offsetHeight / height;
+
+    ctx.beginPath();
+    ctx.moveTo(prevX / scaleX, prevY / scaleY);
+    ctx.lineTo(currentX / scaleX, currentY / scaleY);
+    ctx.stroke();
+
+    setPrevX(currentX);
+    setPrevY(currentY);
+    changes.push([currentX, currentY]);
+  };
+
+  const pushChanges = () => {
+    // get current time, array of pixel changes, color
+    const json = {
+      timestamp: Date.now(),
+      pixels: changes,
+      color: lineColor,
+    };
+    changes = []; // empty out the changes for next push
+  };
+
   return (
-    <div className={styles.canvas}>
+    <div className={styles.canvasWrapper}>
+      <canvas
+        className={styles.canvas}
+        id="canvas"
+        width={width}
+        height={height}
+        ref={canvasRef}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
+        The canvas could not be displayed. Please try again.
+      </canvas>
+      <Palette
+        colors={colors}
+        handleLineColor={setLineColor}
+        widths={widths}
+        handleLineWidth={setLineWidth}
+      />
     </div>
   );
 }
--- frontend/src/components/Palette/Palette.js
+++ frontend/src/components/Palette/Palette.js
@@ -1,5 +1,66 @@
+import { FiPenTool, FiXCircle, FiTrash } from "react-icons/fi";
 import styles from "./Palette.module.css";
 
-export default function Palette() {
-  return <></>;
+export default function Palette({
+  colors,
+  handleLineColor,
+  widths,
+  handleLineWidth,
+}) {
+  return (
+    <div className={styles.palette}>
+      <div className={`${styles.tools} ${styles.paletteSection}`}>
+        <div className={styles.tool}>
+          <FiPenTool />
+        </div>
+        <div className={styles.tool}>
+          <FiXCircle />
+        </div>
+        <div className={styles.tool}>
+          <FiTrash />
+        </div>
+      </div>
+      <div className={`${styles.colorPalette} ${styles.paletteSection}`}>
+        {colors.map((c, i) => (
+          <Color color={c} handleLineColor={handleLineColor} key={i} />
+        ))}
+      </div>
+      <div className={`${styles.stroke} ${styles.paletteSection}`}>
+        {widths.map((w, i) => (
+          <div
+            className={styles.width}
+            onClick={() => handleLineWidth(w)}
+            key={i}
+          >
+            <svg
+              width="14"
+              height="22"
+              viewBox="0 0 14 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="13"
+                y1="1"
+                x2="0.5"
+                y2="21.5"
+                stroke="black"
+                stroke-width={w}
+              />
+            </svg>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Color({ color, handleLineColor }) {
+  return (
+    <div
+      className={styles.color}
+      style={{ background: `${color}` }}
+      onClick={() => handleLineColor(color)}
+    />
+  );
 }
