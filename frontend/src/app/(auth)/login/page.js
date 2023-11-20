8a13ecc4352c30e4fe35183d9af4cce9e39d869c
Anna Hsu <adhsu@usc.edu>
2023-11-19 20:58:23 -0800

    ran prettier

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -7,15 +7,35 @@ export default function LoginPage() {
     <div className={styles.content}>
       <p className={styles.text}>Welcome. Please log in.</p>
       <form id="login" method="get" action="/canvas">
-      <div className={styles.content}>
-      <Input required placeholder="Username" name="username"></Input>
-      <Input required type="password" placeholder="Password" name="password"></Input>
-      </div>
+        <div className={styles.form}>
+          <Input required placeholder="Username" name="username"></Input>
+          <Input
+            required
+            type="password"
+            placeholder="Password"
+            name="password"
+          ></Input>
+        </div>
       </form>
       <div className={styles.buttons}>
-        <Button type="submit" form="login">Login</Button>
+        <Button type="submit" form="login">
+          Login
+        </Button>
       </div>
-      <p className={styles.small}>New member? <span><a href="/signup" className={styles.smalllink}>Create an account</a></span> or <span><a href="/canvas" className={styles.smalllink}>continue as guest</a></span></p>
+      <p className={styles.small}>
+        New member?{" "}
+        <span>
+          <a href="/signup" className={styles.smalllink}>
+            Create an account
+          </a>
+        </span>{" "}
+        or{" "}
+        <span>
+          <a href="/canvas" className={styles.smalllink}>
+            continue as guest
+          </a>
+        </span>
+      </p>
     </div>
   );
 }
--- frontend/src/app/(auth)/signup/page.js
+++ frontend/src/app/(auth)/signup/page.js
@@ -7,19 +7,50 @@ export default function SignupPage() {
     <div className={styles.content}>
       <p className={styles.text}>Create your free account.</p>
       <form id="signup" method="get" action="/canvas">
-      <div className={styles.content}>
-      <Input required placeholder="First name" name="fname"></Input>
-      <Input required placeholder="Last name" name="lname"></Input>
-      <Input required placeholder="Username" name="uname"></Input>
-      <Input required pattern="@[a-z0-9.\-]+\.[a-z]" type="email" placeholder="Email" name="email"></Input>
-      <Input required type="password" placeholder="Password" name="pw"></Input>
-      <Input required type="password" placeholder="Confirm password" name="confirmpw"></Input>
-      </div>
+        <div className={styles.content}>
+          <Input required placeholder="First name" name="fname"></Input>
+          <Input required placeholder="Last name" name="lname"></Input>
+          <Input required placeholder="Username" name="uname"></Input>
+          <Input
+            required
+            pattern="@[a-z0-9.\-]+\.[a-z]"
+            type="email"
+            placeholder="Email"
+            name="email"
+          ></Input>
+          <Input
+            required
+            type="password"
+            placeholder="Password"
+            name="pw"
+          ></Input>
+          <Input
+            required
+            type="password"
+            placeholder="Confirm password"
+            name="confirmpw"
+          ></Input>
+        </div>
       </form>
       <div className={styles.buttons}>
-        <Button type="submit" form="signup">Sign Up</Button>
+        <Button type="submit" form="signup">
+          Sign Up
+        </Button>
       </div>
-      <p className={styles.small}>Already have an account? <span><a href="/login" className={styles.smalllink}>Log in.</a></span><br/> Don't want to sign up? <span><a href="/canvas" className={styles.smalllink}>continue as guest</a></span></p>
+      <p className={styles.small}>
+        Already have an account?{" "}
+        <span>
+          <a href="/login" className={styles.smalllink}>
+            Log in.
+          </a>
+        </span>
+        <br /> Don't want to sign up?{" "}
+        <span>
+          <a href="/canvas" className={styles.smalllink}>
+            continue as guest
+          </a>
+        </span>
+      </p>
     </div>
   );
 }
