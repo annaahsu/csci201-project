7eb9c7231954b8e1a18c89a641cd079ed7a597b0
James Li <jameslicontact@gmail.com>
2023-11-28 19:10:46 -0800

    changes from replit

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -1,39 +1,79 @@
-import Link from "next/link";
+// indicate this program is a client entry so "useState" can be used
+'use client'
+// 3 lines from https://stackoverflow.com/questions/23929432/submit-form-in-reactjs-using-button-element
+import React, { useState } from 'react';
 import Button from "@/components/Button/Button";
-import Input from "@/components/Input/Input";
 import styles from "../styles.module.css";
+import Input from "@/components/Input/Input";
 
 export default function LoginPage() {
-  return (
-    <div className={styles.content}>
-      <p className={styles.text}>Welcome. Please log in.</p>
-      <form id="login" method="get" action="/canvas">
-        <div className={styles.form}>
-          <Input required placeholder="Username" name="username" />
-          <Input
-            required
-            type="password"
-            placeholder="Password"
-            name="password"
-          />
+
+    // declaring variables in the function that will be used for each field in the signup form
+    const [username, recordingUsername] = useState("");
+    const [password, recordingPassword] = useState("");
+
+    // function for fetching from and responding to the backend
+    async function loginFunc(event) {
+        event.preventDefault();
+        console.log({ username, password });
+        var url = new URL("http://localhost:8080/login");
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ username, password })
+        });
+
+        if (response.status === 401) {
+            console.log('Wrong credentials!');
+            document.getElementById("warning_message").innerHTML = '<p style="font-size: 12px">Incorrect Credentials, please try again.</p>';
+            return;
+        }
+        else if (response.status !== 200) {
+            console.error('Unhandled error');
+            console.error(await response.text());
+            return;
+        }
+
+        document.getElementById("warning_message").innerHTML = "";
+        window.location.href = 'http://localhost:3000/canvas';
+
+
+        // Old code
+        //   .then(response => {
+        //     if (!response.ok) {
+        //       throw new Error('Network response was not ok');
+        //     }
+        //     return response.text();
+        //   })
+        //   .then((text) => {
+        //     // Handle the received JSON data
+        //     console.log("here's the text");
+        //     console.log(text); // Check if data is received correctly
+        //     // Update HTML or perform other operations with the data
+        //     //console.log(request.getContextPath() + '/lists.html')
+        //     // window.location.href = request.getContextPath() + '/lists.html';
+        //     if (text == "401 Error") {
+        //       document.getElementById("warning_message").innerHTML = '<p style="font-size: 12px">Incorrect Credentials, please try again.</p>';
+        //     } else {
+        //       document.getElementById("warning_message").innerHTML = "";
+        //       window.location.href = request,getContextPath() + '/canvas';
+        //     }
+        //   })
+        //   .catch(error => console.error('Error fetching data:', error));
+        // console.log("im here!");
+    }
+
+    return (
+        <div className={styles.content}>
+            <p className={styles.text}>Welcome. Please log in.</p>
+            <form id="login" onSubmit={loginFunc}>
+                <div className={styles.content}>
+                    <Input required type="text" placeholder="Username" name="username" value={username} onChange={(event) => recordingUsername(event.target.value)} ></Input>
+                    <Input required type="password" placeholder="Password" name="password" value={password} onChange={(event) => recordingPassword(event.target.value)}></Input>
+                </div>
+            </form>
+            <div id="warning_message" style={{ color: "red" }}></div>
+            <Button type="submit" form="login">Login</Button>
+            <p className={styles.small}>New member? <span><a href="/signup" className={styles.smalllink}>Create an account</a></span> or <span><a href="/canvas" className={styles.smalllink}>continue as guest</a></span></p>
         </div>
-      </form>
-      <div className={styles.buttons}>
-        <Button type="submit" form="login">
-          Login
-        </Button>
-      </div>
-      <p className={styles.small}>
-        New member?{" "}
-        <Link href="/signup" className={styles.smalllink}>
-          Create an account
-        </Link>{" "}
-        or{" "}
-        <Link href="/canvas" className={styles.smalllink}>
-          continue as guest
-        </Link>
-        .
-      </p>
-    </div>
-  );
+    );
 }
--- frontend/src/app/(auth)/signup/page.js
+++ frontend/src/app/(auth)/signup/page.js
@@ -1,50 +1,102 @@
-import Link from "next/link";
+// indicate this program is a client entry so "useState" can be used
+'use client'
+// 3 lines from https://stackoverflow.com/questions/23929432/submit-form-in-reactjs-using-button-element
+import React, { useState } from 'react';
 import Button from "@/components/Button/Button";
-import Input from "@/components/Input/Input";
 import styles from "../styles.module.css";
+import Input from "@/components/Input/Input";
 
 export default function SignupPage() {
-  return (
-    <div className={styles.content}>
-      <p className={styles.text}>Create your free account.</p>
-      <form id="signup" method="get" action="/canvas">
-        <div className={styles.form}>
-          <Input required placeholder="First name" name="fname" />
-          <Input required placeholder="Last name" name="lname" />
-          <Input required placeholder="Username" name="uname" />
-          <Input
-            required
-            pattern="@[a-z0-9.\-]+\.[a-z]"
-            type="email"
-            placeholder="Email"
-            name="email"
-          />
-          <Input required type="password" placeholder="Password" name="pw" />
-          <Input
-            required
-            type="password"
-            placeholder="Confirm password"
-            name="confirmpw"
-          />
+
+    // declaring variables in the function that will be used for each field in the signup form
+    const [fname, recordingFname] = useState("");
+    const [lname, recordingLname] = useState("");
+    const [uname, recordingUname] = useState("");
+    const [email, recordingEmail] = useState("");
+    const [password, recordingPassword] = useState("");
+    const [confirmPassword, recordingConfirmPassword] = useState("");
+
+    // function for fetching from and responding to the backend
+    async function signupFunc(event) {
+        event.preventDefault();
+        console.log({ fname, lname, uname, email, password, confirmPassword });
+        var url = new URL("http://localhost:8080/signup");
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ fname, lname, uname, email, password, confirmPassword })
+        });
+
+        if (response.status === 403) {
+            console.log('Passwords do not match!');
+            document.getElementById("warning_message").innerHTML = '<p style="font-size: 12px">Passwords do not match, please try again.</p>';
+            return;
+        }
+        else if (response.status === 404) {
+            console.log('Username already in use');
+            document.getElementById("warning_message").innerHTML = '<p style="font-size: 12px">User already exists, please try again.</p>';
+            return;
+        }
+        else if (response.status !== 200) {
+            console.error('Unhandled error');
+            console.error(await response.text());
+            return;
+        }
+
+        document.getElementById("warning_message").innerHTML = "";
+        window.location.href = 'http://localhost:3000/canvas';
+
+
+        // Old code
+        // var params = { fname:  fname , lname:  lname , uname:  uname , email:  email , password:  password , confirmPassword:  confirmPassword };
+        // url.search = new URLSearchParams(params).toString();
+        // console.log(url);
+        // console.log(params);
+        // fetch(url)
+        //   .then(response => {
+        //     if (!response.ok) {
+        //       throw new Error('Network response was not ok');
+        //     }
+        //     return response.text();
+        //   })
+        //   .then((text) => {
+        //     // Handle the received JSON data
+        //     console.log("here's the text");
+        //     console.log(text); // Check if data is received correctly
+        //     // Update HTML or perform other operations with the data
+        //     //console.log(request.getContextPath() + '/lists.html')
+        //     // window.location.href = request.getContextPath() + '/lists.html';
+        //     if (text == "Successfully signed up! You can now draw on the canvas!") {
+        //       document.getElementById("warning_message").innerHTML = "";
+        //       window.location.href = request, getContextPath() + '/canvas';
+        //     }
+        //     if (text == "Passwords do not match!") {
+        //       document.getElementById("warning_message").innerHTML = '<p style="font-size: 12px">Passwords do not match, please try again.</p>';
+        //     } else if (text == "Username is already in use.") {
+        //       document.getElementById("warning_message").innerHTML = '<p style="font-size: 12px">User already exists, please try again.</p>';
+        //     } else {
+        //       document.getElementById("warning_message").innerHTML = '<p style="font-size: 12px">Sign up error.</p>';
+        //     }
+        //   })
+        //   .catch(error => console.error('Error fetching data:', error));
+        // console.log("im here!");
+    }
+
+    return (
+        <div className={styles.content}>
+            <p className={styles.text}>Create your free account.</p>
+            <form id="signup" onSubmit={signupFunc}>
+                <div className={styles.content}>
+                    <Input required placeholder="First name" name="fname" value={fname} onChange={(event) => recordingFname(event.target.value)}></Input>
+                    <Input required placeholder="Last name" name="lname" value={lname} onChange={(event) => recordingLname(event.target.value)}></Input>
+                    <Input required placeholder="Username" name="uname" value={uname} onChange={(event) => recordingUname(event.target.value)}></Input>
+                    <Input required type="email" placeholder="Email" name="email" value={email} onChange={(event) => recordingEmail(event.target.value)}></Input>
+                    <Input required type="password" placeholder="Password" name="password" value={password} onChange={(event) => recordingPassword(event.target.value)}></Input>
+                    <Input required type="password" placeholder="Confirm password" name="confirmpassword" value={confirmPassword} onChange={(event) => recordingConfirmPassword(event.target.value)}></Input>
+                </div>
+            </form>
+            <div id="warning_message" style={{ color: "red" }}></div>
+            <Button type="submit" form="signup">Sign Up</Button>
+            <p className={styles.small}>Already have an account? <span><a href="/login" className={styles.smalllink}>Log in.</a></span><br /> Don't want to sign up? <span><a href="/canvas" className={styles.smalllink}>continue as guest</a></span></p>
         </div>
-      </form>
-      <div className={styles.buttons}>
-        <Button type="submit" form="signup">
-          Sign Up
-        </Button>
-      </div>
-      <p className={styles.small}>
-        Already have an account?{" "}
-        <Link href="/login" className={styles.smalllink}>
-          Log in
-        </Link>
-        .
-        <br /> Don&apos;t want to sign up?{" "}
-        <Link href="/canvas" className={styles.smalllink}>
-          continue as guest
-        </Link>
-        .
-      </p>
-    </div>
-  );
+    );
 }
